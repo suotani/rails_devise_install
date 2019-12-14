218c099185a6c9cfea8a3f07dad2b8542994f17f
root <root@DESKTOP-GSID79E.localdomain>
2019-12-15 03:33:56 +0900

    change route after signin and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'mypage', to: "users#show"
-  devise_for :users
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+} 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
